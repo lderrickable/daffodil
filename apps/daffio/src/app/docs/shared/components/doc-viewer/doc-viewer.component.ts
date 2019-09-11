169139ff2b8a1bf3e3ffd80ea0d6ae6786d5ddc4
Damien Retzinger <damienwebdev@gmail.com>
2019-09-11 09:59:56 -0400

    feat(daffio): add prism syntax highlighting

--- apps/daffio/src/app/docs/shared/components/doc-viewer/doc-viewer.component.ts
+++ apps/daffio/src/app/docs/shared/components/doc-viewer/doc-viewer.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { DaffioDoc } from '../../models/doc';
 
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-json';
+
+
 @Component({
   selector: 'daffio-doc-viewer',
   templateUrl: './doc-viewer.component.html',
@@ -12,5 +19,14 @@ export class DaffioDocViewerComponent {
    * The doc to render
    */
   @Input() doc: DaffioDoc;
+
+  highlighted: boolean = false;
+
+  ngAfterViewChecked() {
+    if (this.doc && !this.highlighted) {
+      Prism.highlightAll();
+      this.highlighted = true;
+    }
+  }
 
 }
